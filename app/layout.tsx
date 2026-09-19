7de7e71c7bba5f5f2d50c768ad7cab2a24fe9530
Elena Zen <elenazen563@gmail.com>
2026-09-19 07:51:32 +0200

    Set color variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${fontClasses} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col text-base">{children}</body>
     </html>
   );
 }
