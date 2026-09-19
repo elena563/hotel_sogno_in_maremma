acc6ea8914c93a9ce12b546c311c51a8e720fae8
Elena Zen <elenazen563@gmail.com>
2026-09-19 20:29:19 +0200

    Add shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-
 export const metadata: Metadata = {
   title: "Hotel Sogno in Maremma",
   description: "Un boutique hotel nel cuore della Maremma, Toscana, Italia. Vivi la perfetta combinazione di comfort, eleganza e autentica ospitalità italiana.",
