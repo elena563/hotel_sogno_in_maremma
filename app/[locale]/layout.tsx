4526ab43cb3a26498375e86c33e1fcefb979718f
Elena Zen <elenazen563@gmail.com>
2026-09-19 19:46:49 +0200

    Add i18n

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,61 @@
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
+import { getTranslations } from "next-intl/server";
+import type { Metadata } from "next";
+import { Playfair_Display, Cormorant_Garamond, Lora } from "next/font/google";
+
+const playfairDisplay = Playfair_Display({
+  variable: "--font-playfair-display",
+  subsets: ["latin"],
+});
+
+const cormorantGaramond = Cormorant_Garamond({
+  variable: "--font-cormorant-garamond",
+  subsets: ["latin"],
+});
+
+const lora = Lora({
+  variable: "--font-lora",
+  subsets: ["latin"],
+});
+
+type Props = {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: "Metadata" });
+  return {
+    title: t("title"),
+    description: t("description"),
+  };
+}
+
+export default async function LocaleLayout({ children, params }: Props) {
+  const { locale } = await params;
+
+  if (!routing.locales.includes(locale as (typeof routing.locales)[number])) {
+    notFound();
+  }
+
+  const messages = await getMessages({ locale });
+
+  const fontClasses: string = `${playfairDisplay.variable} ${cormorantGaramond.variable} ${lora.variable}`;
+
+  return (
+    <html
+      lang={locale}
+      className={`${fontClasses} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col text-base">
+    <NextIntlClientProvider messages={messages}>
+      {children}
+    </NextIntlClientProvider>
+    </body>
+    </html>
+  );
+}
